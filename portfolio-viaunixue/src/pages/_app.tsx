a2b0933b899cff6e1dad804b52d154837adae127
JongHyunJung <jjh19960115@gmail.com>
2024-09-18 17:09:15 +0900

    Update DEFAULT SEO

--- portfolio-viaunixue/src/pages/_app.tsx
+++ portfolio-viaunixue/src/pages/_app.tsx
@@ -7,21 +7,21 @@ import type { AppProps } from "next/app";
  * @description SEO를 위해 본인의 정보로 수정해주세요.
  */
 const DEFAULT_SEO = {
-  title: "홍길동 | Front-End Dev",
-  description: "안녕하세요, 프론트엔드 개발자 홍길동입니다.",
+  title: "정종현 | Software Dev",
+  description: "안녕하세요, 백엔드 개발자 정종현입니다.",
   canonical: "https://www.naver.com/",
   openGraph: {
     type: "website",
     locale: "ko_KR",
     url: "https://www.naver.com/",
-    title: "홍길동 | Front-End Dev",
-    site_name: "홍길동 | Front-End Dev",
+    title: "정종현 | Software Dev",
+    site_name: "정종현 | Software Dev",
     images: [
       {
         url: "/share.png",
         width: 285,
         height: 167,
-        alt: "홍길동 | Front-End Dev",
+        alt: "정종현 | Software Dev",
       },
     ],
   },
@@ -34,11 +34,11 @@ const DEFAULT_SEO = {
   additionalMetaTags: [
     {
       name: "application-name",
-      content: "홍길동 | Front-End Dev",
+      content: "정종현 | Software Dev",
     },
     {
       name: "msapplication-tooltip",
-      content: "홍길동 | Front-End Dev",
+      content: "정종현 | Software Dev",
     },
     {
       name: "viewport",
