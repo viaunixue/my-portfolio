1e5ee525bf5024001e4bad3d469e55384f8f6c80
JongHyunJung <jjh19960115@gmail.com>
2024-09-19 15:48:42 +0900

    Add Introduce Image

--- portfolio-viaunixue/src/components/Information/index.tsx
+++ portfolio-viaunixue/src/components/Information/index.tsx
@@ -1,29 +1,37 @@
 import ContactItem from "../ContactItem";
 import Introduce from "./Introduce";
+import Image from 'next/image';
 
 import { DataProps } from "@/types";
 
 const Information = ({ information }: Pick<DataProps, "information">) => {
   return (
     <div className="flex flex-col gap-8">
-      <div className="flex flex-col gap-2">
-        <h1 className="leading-[1.15]">
-          {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          안녕하세요,
-          <br /> 백엔드 개발자{" "}
-          <span className="text-PRIMARY font-semibold">{information.name}</span>
-          입니다.
-        </h1>
-        <div className="flex gap-1">
-          {information.contact.map((contact) => (
-            <ContactItem
-              key={contact.id}
-              className="text-BLACK hover:text-PRIMARY_HEAVY dark:hover:text-PRIMARY_HEAVY"
-              {...contact}
-            >
-              {contact.name}
-            </ContactItem>
-          ))}
+      <div className="flex flex-col md:flex-row items-center md:items-start gap-8">
+        <div className="w-40 h-40 rounded-full overflow-hidden flex-shrink-0">
+          <Image
+            src="/images/profile.png"
+            alt="Profile picture"
+            width={135}
+            height={160}
+            className="object-cover"
+          />
+        </div>
+        <div className="flex flex-col gap-4 items-center md:items-start">
+          <h1 className="leading-[1.15] text-center md:text-left">
+            안녕하세요,
+            <br /> 백엔드 개발자{" "}
+            <span className="text-PRIMARY font-semibold">{information.name}</span>
+            입니다.
+          </h1>
+          <div className="flex gap-4">
+            {information.contact.map((contact) => (
+              <ContactItem
+                key={contact.id}
+                {...contact}
+              />
+            ))}
+          </div>
         </div>
       </div>
       <Introduce markdown={information.markdown} />
